afe4f3901e2315c76f8a64acd1e80c8a96ed53d5
Diego <diego-fidalgo@hotmail.com>
2019-06-02 02:06:45 -0300

    Adicionadas rotas de fetch

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 const express = require('express');
-
-let public_dir = 'public';
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
-app.use(express.static(public_dir));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/fetch_user_info',(req,res) => {
+    fs.readFile(`info/${req.query.id}.json`,'utf-8',(err,data) => {
+        res.json(data);
+    });
+});
+
+app.get('/get_event_data',(req,res) => {
+    fs.readdir(`event_db/${req.query.id}`,(err,files) => {
+        if (err) throw err;
+
+        let promises = [];
+
+        for (file of files){
+            promises.push(fetch_event_data(req.query.id,file))
+        }
+
+        Promise.all(promises)
+        .then((events) => {
+            res.json(events);
+        })
+        .catch((err) => {
+            res.send(null);
+        })
+    });
+});
 
 app.listen(3000,() => {
     console.log('Server Started');
 });
+
+function fetch_event_data(iduser,eventfile){
+    return new Promise ((resolve,reject) => {
+        fs.readFile(`event_db/${iduser}/${eventfile}`,'utf-8',(err,data) => {
+            if (err) reject(null);
+            resolve(data);
+        });
+    });
+}
